44bdc30a556fcb4233d3f33c990422a6faa32eaa
Mike Shelby <mikegshelby@gmail.com>
2020-10-25 15:11:04 -0500

    add styles and formatting

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var currentDate = moment().format("dddd" + ", " + "MMMM DD");
+
+// add current day and date to header
+$('#currentDay').text(currentDate);
+
+// get current time
+var currentTime = moment().format("hh a");
+console.log(currentTime);
+
+// get task time
+
+// compare current time to task time
+
